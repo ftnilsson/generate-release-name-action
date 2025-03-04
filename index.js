d1208a7e979a987ce5c069a54f0d17acde642c60
Fredrik Nilsson <711200+ftnilsson@users.noreply.github.com>
2025-03-04 10:48:03 +0100

    Update index.js
    updated index.js with logging

--- index.js
+++ index.js
@@ -8,12 +8,13 @@ try {
   const length = core.getInput('length');
   const useToken = core.getInput('useToken');
   const capitalize = core.getInput('capitalize');
-
+  console.log(`input parameters: ${delimiter} ${length} ${useToken} ${capitalize}`); 
   const releaseName = generate(delimiter, length, useToken, capitalize);
   core.setOutput("release-name", releaseName);
+  console.log(`the release-name: ${releaseName}`); 
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  // const payload = JSON.stringify(github.context.payload, undefined, 2)
+  // console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
